077c7b8541483162c72f9730c203eaf02dbcd099
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-02-12 19:45:20 +0200

    187

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+# models.py
+
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blog_images/')
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import BlogPost
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def blog(request):
-    return render(request, 'blog/blog.html')
+    
+       # Retrieve all posts and order them by created_at descending
+    posts = BlogPost.objects.all().order_by('-created_at')
+    
+    # Create a Paginator object with 5 posts per page
+    paginator = Paginator(posts, 1)  # Show 5 blog posts per page
+
+    page = request.GET.get('page')  # Get the page number from the query string
+    try:
+        posts = paginator.page(page)  # Get the posts for the requested page
+    except PageNotAnInteger:
+        # If the page is not an integer, return the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If the page is out of range, return the last page
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/blog.html',{'posts': posts,'paginator': paginator})
